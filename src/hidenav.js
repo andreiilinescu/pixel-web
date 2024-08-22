c7fee495394ece0034b512288424544fc8d36a9f
Andrei <andreiilinescu02@gmail.com>
2024-08-22 23:53:20 +0300

    minor changes to nav

--- src/hidenav.js
+++ src/hidenav.js
@@ -2,9 +2,13 @@ var prevScrollpos = window.scrollY;
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").classList.remove("fade-in-image");
-  } else {
+    document.getElementById("navbar").classList.remove("fade-out-image");
     document.getElementById("navbar").classList.add("fade-in-image");
+
+  } else {
+    document.getElementById("navbar").classList.add("fade-out-image");
+    document.getElementById("navbar").classList.remove("fade-in-image");
+
   }
   prevScrollpos = currentScrollPos;
 }
